42150a020f77b7cb937af2bb2e261a1882f71758
Lukas Krimphove <lukas.krimphove@gmx.de>
2024-02-20 17:19:54 +0100

    fix: :adhesive_bandage: fixed broken links coming from Hashnode dashboard

--- next.config.js
+++ next.config.js
@@ -2,6 +2,35 @@ const HASHNODE_ANALYTICS_BASE_URL = "https://hn-ping2.hashnode.com";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/blog/badges",
+        destination: "/badges",
+        permanent: true,
+      },
+      {
+        source: "/blog/newsletter",
+        destination: "/newsletter",
+        permanent: true,
+      },
+      {
+        source: "/blog/sponsor",
+        destination: "/support",
+        permanent: true,
+      },
+      {
+        source: "/blog/sitemap.xml",
+        destination: "/sitemap.xml",
+        permanent: true,
+      },
+      {
+        source: "/blog/rss.xml",
+        destination: "/rss.xml",
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
